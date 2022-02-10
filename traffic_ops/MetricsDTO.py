7125461bcc87ddf850c9eda98c4927b3ca96ffac
Joshua Zenn <wordman05@gmail.com>
2022-02-10 15:33:28 -0500

    Elasticsearch fixes

--- traffic_ops/MetricsDTO.py
+++ traffic_ops/MetricsDTO.py
@@ -1,7 +1,11 @@
+from datetime import datetime, timezone, timedelta
+
 class MetricsDTO:
     def __init__(self, name: str, data: dict, mappings: list):
         self.__data = dict()
         data['hostname'] = name
+        data['check_time'] = datetime.now(timezone.utc).isoformat()
+        data['next_check'] = (datetime.now(timezone.utc) + timedelta(seconds=10)).isoformat()
         for mapping in mappings:
             self.__data[mapping['output']] = data[mapping['input']]
 
